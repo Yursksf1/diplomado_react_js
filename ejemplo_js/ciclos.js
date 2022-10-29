a1d6e20c3bd7cad4e1e3ea39c1b68e01b465cfdc
yurley <yurs.ksf1@gmail.com>
2022-10-28 20:29:02 -0500

    ciclos -- while, ejercicio

--- ejemplo_js/ciclos.js
+++ ejemplo_js/ciclos.js
@@ -1,6 +1,6 @@
 inicio = 0
-fin = 10
-fin = parseInt(window.prompt('ingrese el numero para hacer el ciclo: '))    
+fin = 0
+// fin = parseInt(window.prompt('ingrese el numero para hacer el ciclo: '))    
 
 for (let index = inicio; index < fin; index++) {
     console.log(index)
@@ -27,3 +27,39 @@ console.log('se acabo la ejecucion de mi codigo')
  * 
  * promedio: 10 
  */
+
+
+// WHILE
+
+contador = 0
+bandera = true
+while (bandera) {
+    console.log('estoy dentro de un bucle w')
+    console.log(contador)
+    contador = contador + 1
+    if (contador > 10) {
+        // bandera = false
+        break
+    }
+    value = window.prompt('insert de control: ')
+    // console.log(value)
+}
+console.log('ya sali del ciclo while')
+
+
+/**
+ * Realizar un programa en Js
+ * que calcule la suma de los valores ingresados por un alert
+ * ya que el programa no es conciente de cuantos valores seran agregados
+ * el programa dejara de sumar y recibir numeros cuando el valor ingresado sea 0
+ * 
+ * Ingresa un numero: 5
+ * Ingresa un numero: 4
+ * Ingresa un numero: 3
+ * Ingresa un numero: 0
+ * 
+ * El valor total es: 12
+ * 
+ */
+
+
