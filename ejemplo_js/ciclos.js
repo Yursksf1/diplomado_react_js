701d271570de0b869f651251af6ce67da9a21b6e
yurley <yurs.ksf1@gmail.com>
2022-10-28 19:37:23 -0500

    ciclos -- for, ejercicio

---
+++ ejemplo_js/ciclos.js
@@ -0,0 +1,26 @@
+inicio = 0
+fin = 10
+
+for (let index = inicio; index < fin; index++) {
+    console.log(index)
+    
+}
+console.log('se acabo la ejecucion de mi codigo')
+
+
+/**
+ * Ejeccicio 
+ * Hacer un programa en Js que pregunte al usuario cuantos valores desea ingresar 
+ * dichos valores deben ser numericos.
+ * cuando termine de ingresar los valores el programa debe visualizar el promedio.
+ * 
+ * Ejemplo:
+ * valores a ingresar: 3
+ * valor 1: 5
+ * valor 2: 10
+ * valor 3: 15
+ * 
+ * operacion = (5 + 10 + 15) / 3  ==> 10 (promedio)
+ * 
+ * promedio: 10 
+ */
