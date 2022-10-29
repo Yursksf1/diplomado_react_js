dd857b1a48adc4d01076ba0585e069ef5dc4f065
yurley <yurs.ksf1@gmail.com>
2022-10-28 20:58:29 -0500

    ciclos -- while, ejercicio

--- ejemplo_js/ciclos.js
+++ ejemplo_js/ciclos.js
@@ -63,3 +63,37 @@ console.log('ya sali del ciclo while')
  */
 
 
+
+/**
+ * 
+ * Ejercicio de Interes
+
+"""
+Un prestamos de 10'000.000 a una tasa de interes del 1.5% NMV 
+significa que mensualmente esta deuda generara interes de 150.000
+es decir al final de mes tienes una deunda de: 10'150.000
+
+asumiendo que haces un abono de 500.000 pesos mensual. en cuanto tiempo 
+terminas de saldar la deuda? 
+
+deuda: 10'000.000
+mes 1: 10'150.000 - 500.000 = 9'650.000
+mes 2: 9'650.000 + 150.000 - 500.000 = 9'300.000
+....
+
+mes n: 9'650.000 + 150.000 - 500.000 = 0 
+"""
+
+ * 
+ */
+
+/**
+ * proponer una applicacion que permita poner el terminos de variables el ejercicio anterior
+ * monto_prestamo --> ingresa el usuario
+ * tasa_de_interes --> ingresa el usuario
+ * abono_mensual --> ingresa el usuario
+ * 
+ * cantidad_de_meses --> resultado del programa
+ * pago_total_de_la_deuda --> resultado del programa
+ * 
+ */
