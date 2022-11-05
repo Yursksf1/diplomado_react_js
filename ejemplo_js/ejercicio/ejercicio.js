847166e44842ae5de252ab0a63290c5331dd9077
yurley <yurs.ksf1@gmail.com>
2022-11-04 21:06:08 -0500

    use jquery

--- ejemplo_js/ejercicio/ejercicio.js
+++ ejemplo_js/ejercicio/ejercicio.js
@@ -1,17 +1,18 @@
 
-document.getElementById("tabla_1").addEventListener("click", function() { tabla(1); } )
-document.getElementById("tabla_2").addEventListener("click", function() { tabla(2); } )
-document.getElementById("tabla_3").addEventListener("click", function() { tabla(3); } )
-document.getElementById("tabla_4").addEventListener("click", function() { tabla(4); } )
-document.getElementById("tabla_5").addEventListener("click", function() { tabla(5); } )
-document.getElementById("tabla_6").addEventListener("click", function() { tabla(6); } )
+$("#tabla_1").click(function() { tabla(1); } )
+$("#tabla_2").click(function() { tabla(2); } )
+$("#tabla_3").click(function() { tabla(3); } )
+$("#tabla_4").click(function() { tabla(4); } )
+$("#tabla_5").click(function() { tabla(5); } )
+$("#tabla_6").click(function() { tabla(6); } )
 
 function tabla(param){
-    document.getElementById("multiply").innerHTML = ""
+    console.log("param", param)
+    $("#multiply").text("")
     array = [1,2,3,4,5,6,7,8,9,10]
     array.forEach(element => {
         // text = element + " * " + param + " = " + (element*param) + "<br>"
         text = `${element} * ${param} = ${element*param} </br>`
-        document.getElementById("multiply").innerHTML += text
+        $("#multiply").html($("#multiply").html() +text)
     });
 }
