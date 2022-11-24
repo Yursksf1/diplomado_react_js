f0cbf69e7ccaa711edf833b0112bd0fa3de9e89c
yurley <yurs.ksf1@gmail.com>
2022-11-23 19:42:40 -0500

    publish exaple react

---
+++ example_react_project/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Car from './components/Car.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> saludos a toda la clase </h1>
+      <Car/> 
+    </div>
+  );
+}
+
+export default App;
