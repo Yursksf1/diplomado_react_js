dc829b2a8674019c2e2e126ec572912fe72aa424
Andres Parra <andresparra@gmail.com>
2022-11-29 19:05:54 -0500

    actividad cursos finalizada

--- actividad-cursos/src/App.js
+++ actividad-cursos/src/App.js
@@ -1,133 +1,109 @@
 import "./App.css";
 import React from "react";
 
-function Video(props) {
-  const { video } = props
+function Titulo(props) {
+  const { title } = props;
   return (
-    <button
-    type="button"
-    class="list-group-item list-group-item-action fs-5"
-    >
-      <img
-        className="img-fluid icon-list me-3"
-
-        alt=""
-      ></img>
-      {video}
-    </button>
-  )
-}
-
-function Videos(props) {
-  const {videos} = props
-  const lista =  videos.map( (video) => {
-    return <Video video={video} />
-  } 
-)
-return (
-  <div>
-    {lista}
-  </div>
-  )
+    <ul className="nav nav-tabs border border-0" id="myTab" role="tablist">
+      <li className="nav-item" role="presentation">
+        <button className="nav-link active fw-bold fs-5 mt-5 rounded-pill border border-0 bg-info text-white shadow-lg border ms-4 mb-3" >{title}</button>
+      </li>
+    </ul>
+  );
 }
 
 function Cursos(props) {
-  const { title, description, imagen, videos, idElement, idTarget} = props;
-  return(
-    <div class="accordion-item shadow-lg">
-    <h1 class="accordion-header" id={idElement}>
-      <button
-        class="accordion-button collapsed fs-4 text-black fw-semibold bg-green text-white"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target={`#${idTarget}`}
-        aria-expanded="false"
-        aria-controls={idTarget}
-      >
-        {title}
-      </button>
-    </h1>
-    <div
-      id={idTarget}
-      class="accordion-collapse collapse"
-      aria-labelledby={idElement}
-      data-bs-parent="#accordionExample"
-    >
-      <div class="accordion-body">
-        <img
-          className="img-fluid w-50 rounded-4  mt-5 mb-5 mx-auto d-flex shadow1"
-          src={imagen}
-          alt=""
-        ></img>
-        <h2 className="mt-2 mb-4">Videos</h2>
-        <div class="list-group">
-        <Videos
-          videos={videos}
-        />
-        </div>
-        <h2 className="mt-4 mb-4">Descripción</h2>
-        <p className="mb-4 fs-5">
-          {description}
-
-          <br></br>
-        </p>
-      </div>
-    </div>
-  </div>
-  )
-}
-
-
-
-function App() {
+  const { title, description, video } = props;
   return (
-    <div className="App mt-5">
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link active fw-bold fs-5"
-            id="home-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#home-tab-pane"
-            type="button"
-            role="tab"
-            aria-controls="home-tab-pane"
-            aria-selected="true"
-          >
-            fundamentos de musica
-          </button>
-        </li>
-      </ul>
-      <div className="tab-content" id="myTabContent">
-        <div
-          className="tab-pane fade show active"
-          id="home-tab-pane"
-          role="tabpanel"
-          aria-labelledby="home-tab"
-          tabindex="0"
-        >
-          <div className="card w-25 mt-2 ms-2">
-            <div className="card-body">
-              <h5 className="card-title">introduccion</h5>
-              <iframe
-                width="300"
-                height="250"
-                src="https://www.youtube.com/embed/3m2k16uKbwM"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
-            <h5>Descripcion:</h5>
-            <p>En esta clase aprenderas conseptos basicos de musica.</p>
-          </div>
-        </div>
+    <div className="card w-25 mt-2 ms-4 me-4 shadow-lg rounded-5">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <iframe
+          width="300"
+          height="250"
+          src={video}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <h5>Descripcion:</h5>
+        <p>{description}</p>
       </div>
     </div>
   );
 }
 
-
+function App() {
+  return (
+    <div className="App mt-2 mb-4">
+      <Titulo title="Fundamentos de Musica" />
+      <div className="tab-content d-flex" id="myTabContent">
+        <Cursos
+          title="introduccion"
+          description="en esta clase aprenderas conseptos basicos de musica."
+          video="https://www.youtube.com/embed/3m2k16uKbwM"
+        />
+        <Cursos
+          title="Ritmo, Melodía y Armonía"
+          description="En este video te cuento como funciona la música, te explico a detalle los conceptos de melodía, armonía y ritmo (conceptos básicos de fl studio), con ayuda del programa de producción musical fl estudio."
+          video="https://www.youtube.com/embed/VgALuTc-j8U"
+        />
+      </div>
+      {/* curso 2 */}
+      <Titulo title="Tecnica Vocal" />
+      <div className="tab-content d-flex" id="myTabContent">
+        <Cursos
+          title="Técnica Vocal - Ejercicios para Afinar la Voz - Mejora Tu Voz"
+          description="Descubre cómo cantar mejor controlando la afinación de la voz. Afinar la voz significa cantar en el tono adecuado. A veces tenemos dificultad porque no nos damos cuenta de nuestros errores. Estos ejercicios te ayudarán a controlar tu afinación de manera sencilla y progresiva.."
+          video="https://www.youtube.com/embed/G2vDNamPNoc"
+        />
+        <Cursos
+          title="Clases de canto 1 | Como cantar Bien "
+          description="una clase de canto corta con el propósito de trabajar"
+          video="https://www.youtube.com/embed/UVUwP8AqD1g"
+        />
+        <Cursos
+          title="Como cantar Raspado"
+          description="Como Raspar la voz al cantar | Rasgado Clases de Canto Gutural"
+          video="https://www.youtube.com/embed/0DM9yUq4Pjk"
+        />
+      </div>
+      {/* curso 3 */}
+      <Titulo title="Tocar Guitarra" />
+      <div className="tab-content d-flex" id="myTabContent">
+        <Cursos
+          title="¡Aprende como tocar guitarra en solo 20 minutos! El mejor tutorial para principiantes"
+          description="Como tocar guitarra fácil! Aprende 8 acordes básicos en 10 minutos!"
+          video="https://www.youtube.com/embed/S_QIaGgLJ-0"
+        />
+        <Cursos
+          title="Como tocar guitarra fácil! Aprende 8 acordes básicos en 10 minutos!"
+          description="Como tocar guitarra fácil! Aprende 8 acordes básicos en 10 minutos!"
+          video="https://www.youtube.com/embed/kdHMt6H4kDc"
+        />
+      </div>
+      {/* curso 4 */}
+      <Titulo title="Tocar piano" />
+      <div className="tab-content d-flex" id="myTabContent">
+        <Cursos
+          title="Como Tocar El Piano / Fàcil y Ràpido / Para Principiantes"
+          description="Piano facil y divertidas"
+          video="https://www.youtube.com/embed/jaDOE_cYxSQ"
+        />
+        <Cursos
+          title="partiduras"
+          description="Pantera rosa"
+          video="https://www.youtube.com/embed/UX55giqXlZY"
+        />
+        <Cursos
+          title="COMO TOCAR navidad"
+          description="musida de navidad"
+          video="https://www.youtube.com/embed/eJELRZrrLSk"
+        />
+      </div>
+    </div>
+  );
+}
 
 export default App;
