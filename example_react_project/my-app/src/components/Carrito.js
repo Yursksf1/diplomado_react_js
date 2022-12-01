c8403c98ea79bde417cd830c440b55f1fec9a091
yurley <yurs.ksf1@gmail.com>
2022-11-30 19:49:11 -0500

    chages state

--- example_react_project/my-app/src/components/Carrito.js
+++ example_react_project/my-app/src/components/Carrito.js
@@ -11,19 +11,46 @@ class Carrito extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      color: "black"
     };
   }
-  changeColor = () => {
-    this.setState({color: "blue"});
+
+  changeColor = (color) => {
+    console.log('test')
+    this.setState({color: color});
   }
 
+
   render() {
+    const { color } = this.state
+    let nombre_clase = 'contenido_color_black'
+
+    if (color=="blue") {
+      nombre_clase = 'contenido_color_blue'
+    } else if (color=="yellow") {
+      nombre_clase = 'contenido_color_yellow'
+
+    }
+     
     return (
       <div className='container'>
         <h1>Mi lista de compras</h1>
         <ul className="list-group">
           <Item item={'manzana'} imagen={manzana} /> 
           <Item item={'pera'} imagen={pera}/> 
+          <button
+            onClick={() => this.changeColor("blue")}
+        >
+        change color blue
+      </button>
+      <button
+            onClick={() => this.changeColor("yellow")}
+        >
+        change color yellow
+      </button>
+          <p className={nombre_clase}>
+            {color}
+          </p>
           {/* agregar items: uvas, fresas, piña */}
         </ul>
 
